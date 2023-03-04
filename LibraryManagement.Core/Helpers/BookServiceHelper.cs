cc2b395e334947da8cf10c763acf4b648f0b1ff0
semih <semih_guzel@outlook.com>
2023-03-04 22:29:16 +0300

    helper for BookService added

---
+++ LibraryManagement.Core/Helpers/BookServiceHelper.cs
@@ -0,0 +1,20 @@
+﻿using LibraryManagement.Core.Entities;
+
+namespace LibraryManagement.Core.Helpers;
+
+public static class BookServiceHelper
+{
+    public static void ThrowExceptionIfArgIsNull(Book book)
+    {
+        if (book == null)
+            throw new ArgumentNullException();
+    }
+
+    public static void CheckArgsForException(Book book)
+    {
+        if (string.IsNullOrWhiteSpace(book.Name) || string.IsNullOrWhiteSpace(book.Author) || book.Quantity == 0 ||
+            book.PageNumber == 0 || book.BookCategories.Count == 0 || book.CreatedDate == DateTime.MinValue)
+            throw new ArgumentException();
+    }
+    
+}
