6da7a8b1c0c3677bc6bce021a8bf8437c1a9e917
semih <semih_guzel@outlook.com>
2023-03-04 22:31:27 +0300

    BookRepositoryTests added

---
+++ LibraryManagement.UnitTests/Repository/Book/BookRepositoryTests.cs
@@ -0,0 +1,61 @@
+﻿using LibraryManagement.Core.Interfaces.Repositories;
+using LibraryManagement.Infrastructure.Repositories;
+using Moq;
+using NUnit.Framework;
+
+namespace LibraryManagement.UnitTests.Repository.Book;
+
+[TestFixture]
+public class BookRepositoryTests
+{
+    private IBookRepository _bookRepository;
+    private Mock<IBookRepository> _bookRepositoryMock;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _bookRepository = new BookRepository(null);
+        _bookRepositoryMock = new Mock<IBookRepository>();
+    }
+
+    /*
+     * GetByName
+     */
+
+    /*
+     * GetByName => name parametresi;
+     *                  -boş olabilir
+     *                  -dolu ve veritabanında olabilir
+     *                  -dolu ve veritabanında olmayabilir
+     */
+
+    [TestCase("")]
+    [TestCase(null)]
+    [TestCase("              ")]
+    public void GetByName_NullOrWhiteSpace_ArgumentException(string name)
+    {
+        Assert.ThrowsAsync<ArgumentException>(() => _bookRepository.GetByName(name));
+    }
+
+    [Test]
+    public async Task GetByName_NotExisting_ReturnNull()
+    {
+        string name = "Silmarillion";
+        Core.Entities.Book result = null;
+
+        _bookRepositoryMock.Setup(x => x.GetByName(name)).ReturnsAsync(result);
+
+        Assert.That(() => _bookRepositoryMock.Object.GetByName(name), Is.EqualTo(result));
+    }
+    
+    [Test]
+    public async Task GetByName_ExistingName_ReturnItem()
+    {
+        string name = "Silmarillion";
+        Core.Entities.Book result = new Core.Entities.Book {Name = "Silmarillion"};
+
+        _bookRepositoryMock.Setup(x => x.GetByName(name)).ReturnsAsync(result);
+
+        Assert.That(() => _bookRepositoryMock.Object.GetByName(name), Is.EqualTo(result));
+    }
+}
