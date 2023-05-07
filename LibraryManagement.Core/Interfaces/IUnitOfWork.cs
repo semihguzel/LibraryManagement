c74e70769b59686601c75a54062560eaab448092
semih <semih_guzel@outlook.com>
2023-05-07 16:37:47 +0300

    UnitOfWork implemented

---
+++ LibraryManagement.Core/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using LibraryManagement.Core.Interfaces.Repositories;
+
+namespace LibraryManagement.Core.Interfaces;
+
+public interface IUnitOfWork
+{
+    IBookRepository BookRepository { get; }
+    IBookCategoryRepository BookCategoryRepository { get; }
+    ILoanRepository LoanRepository { get; }
+    IUserRepository UserRepository { get; }
+    IUserRoleRepository UserRoleRepository { get; }
+    int Save();
+    Task<int> SaveAsync();
+}
---
+++ LibraryManagement.Infrastructure/UnitOfWork.cs
@@ -0,0 +1,47 @@
+﻿using LibraryManagement.Core.Interfaces;
+using LibraryManagement.Core.Interfaces.Repositories;
+using LibraryManagement.Infrastructure.Repositories;
+
+namespace LibraryManagement.Infrastructure;
+
+public class UnitOfWork : IUnitOfWork
+{
+    private readonly LibraryContext _context;
+
+    public UnitOfWork(LibraryContext context)
+    {
+        _context = context;
+        BookRepository = new BookRepository(_context);
+        BookCategoryRepository = new BookCategoryRepository(_context);
+        LoanRepository = new LoanRepository(_context);
+        UserRepository = new UserRepository(_context);
+        UserRoleRepository = new UserRoleRepository(_context);
+    }
+
+
+    public IBookRepository BookRepository { get; }
+    public IBookCategoryRepository BookCategoryRepository { get; }
+    public ILoanRepository LoanRepository { get; }
+    public IUserRepository UserRepository { get; }
+    public IUserRoleRepository UserRoleRepository { get; }
+
+    public int Save()
+    {
+        return _context.SaveChanges();
+    }
+
+    public async Task<int> SaveAsync()
+    {
+        return await _context.SaveChangesAsync();
+    }
+
+    public void Dispose()
+    {
+        _context.Dispose();
+    }
+
+    public async Task DisposeAsync()
+    {
+        await _context.DisposeAsync();
+    }
+}
