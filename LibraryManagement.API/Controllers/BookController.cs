5e1d6961e49312287b46fb2bfa5d0174ae9e32cb
semih <semih_guzel@outlook.com>
2023-05-14 15:41:17 +0300

    BookController added

---
+++ LibraryManagement.API/Controllers/BookController.cs
@@ -0,0 +1,44 @@
+﻿using System.Net;
+using AutoMapper;
+using LibraryManagement.API.Helpers;
+using LibraryManagement.Core.Entities;
+using LibraryManagement.Core.Interfaces.Services;
+using LibraryManagement.Service.Book.Dto;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryManagement.API.Controllers;
+
+public class BookController : BaseController
+{
+    private readonly IMapper _mapper;
+    private IBookService _bookService;
+
+    public BookController(IMapper mapper, IBookService bookService)
+    {
+        _mapper = mapper;
+        _bookService = bookService;
+    }
+
+    public async Task<IActionResult> AddBooks(BookCreateUpdateDto dto)
+    {
+        try
+        {
+            var book = _mapper.Map<Book>(dto);
+
+            await _bookService.Add(book);
+            return StatusResult(new ResponseBody<Book>() { Item = book }, HttpStatusCode.OK);
+        }
+        catch (ArgumentNullException e)
+        {
+            return StatusResult(new ResponseBody<Book> { Message = e.Message }, HttpStatusCode.BadRequest);
+        }
+        catch (ArgumentException e)
+        {
+            return StatusResult(new ResponseBody<Book> { Message = e.Message }, HttpStatusCode.BadRequest);
+        }
+        catch (Exception e)
+        {
+            return StatusResult(new ResponseBody<Book> { Message = e.Message }, HttpStatusCode.InternalServerError);
+        }
+    }
+}
---
+++ LibraryManagement.Service/Book/Dto/BookCreateUpdateDto.cs
@@ -0,0 +1,11 @@
+﻿namespace LibraryManagement.Service.Book.Dto;
+
+public class BookCreateUpdateDto
+{
+    public string Name { get; set; }
+    public string Author { get; set; }
+    public int PageNumber { get; set; }
+    public int Quantity { get; set; }
+
+    public Guid BookCategoryId { get; set; }
+}
